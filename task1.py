5177984df3d019b9b144a6cf4933011263efa288
unknown <timone300003@gmail.com>
2023-07-06 19:58:44 +0300

    сделали первое задание

--- task1.py
+++ task1.py
@@ -6,11 +6,14 @@
 #     A = 2; B = 3 -> 8 
 
 
-def step_num(num, num2):
-    steping_num = num ** num2
-    return steping_num
+def step_num(num, end):
+    if end == 0:
+        return 1
+    else:
+        return num * step_num(num, end - 1)
 
 
+        
 A = int(input('Введите A: '))
-B = int(input('Введите B: '))
+B = int(input('Введите степень B: '))
 print(f'A = {A}, B = {B} -> {step_num(A, B)}')
